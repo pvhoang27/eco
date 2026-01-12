cb60dee6e94a6769f4719e5499dea276819340de
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-01-12 14:56:04 +0700

    2

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,9 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Header from './components/Header';
+import CategoryMenu from './components/CategoryMenu';
+import FlashSale from './components/FlashSale';
 
 function App() {
   const [cart, setCart] = useState([]);
@@ -32,6 +35,9 @@ function App() {
 
   return (
     <div className="App" style={{ fontFamily: 'Roboto, Arial, sans-serif', background: '#f5f5f5', minHeight: '100vh' }}>
+      <Header />
+      <CategoryMenu />
+      <FlashSale />
       <Navbar user={user} onLoginClick={() => setPage("login")} cartCount={cart.length} />
       {page === "home" && (
         <>
